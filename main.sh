2febd2ec030a6b217df12bdaf1242e390a56a752
Hood Chatham <roberthoodchatham@gmail.com>
2025-08-15 17:19:33 +0200

    try to fix build for rust 2025-06-27

--- main.sh
+++ main.sh
@@ -7,4 +7,9 @@ fi
 ./emsdk/emsdk install $1
 ./emsdk/emsdk activate $1
 source ./emsdk/emsdk_env.sh
+
+# Don't tell Rust it's running in CI or it gets mad that:
+# "`llvm.download-ci-llvm` cannot be set to `true` on CI. Use `if-unchanged` instead."
+unset GITHUB_ACTIONS
+
 python3 main.py $@
